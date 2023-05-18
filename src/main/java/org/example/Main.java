192258d78882aa2c258dea06dabb216c1b2a32b3
Mike-P63 <rumaish@rambler.ru>
2023-05-18 22:28:51 +0400

    Seminar 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Task_1 task_1 = new Task_1();
+        task_1.reversePhrase("Тестовая строка");
+    }
+}
---
+++ src/main/java/org/example/Task_1.java
@@ -0,0 +1,16 @@
+//Во фразе "Добро пожаловать на курс по Java"
+// переставить слова в обратном порядке.
+
+package org.example;
+
+import java.util.Arrays;
+
+public class Task_1 {
+    public void reversePhrase(String phrase){
+        String[] array = phrase.split(" ");
+        //       System.out.println(Arrays.toString(array));
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i] + " ".trim());
+        }
+    }
+}
